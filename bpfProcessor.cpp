920a0c04d8104c4d08dea108da7aa1db287c602a
sol <msfontenla@fi.uba.ar>
2020-11-13 11:27:56 -0300

    cambio condicion de while

--- bpfProcessor.cpp
+++ bpfProcessor.cpp
@@ -1,21 +1,25 @@
 #include "bpfProcessor.h"
+#include "dfs.h"
 
 void BpfProcessor::run(){
   std::string archivo_procesado =
   this->archivos.entregar_dato_si_no_esta_vacio();
-  while (archivo_procesado.size() > 1){
+  while (archivo_procesado != ""){
     Parser parser = Parser(archivo_procesado);
     Grafo grafo = Grafo();
     bool pude_inicializar = parser.run(grafo);
     if (grafo.cantidad_nodos() == 0 || !pude_inicializar){
       return;
     }
-    Detector detector = Detector();
     std::string resultado(archivo_procesado);
-    if (detector.detecto_ciclos(grafo)){
+    Dfs dfs = Dfs();
+    bool hay_ciclo = dfs.realizar_recorrido(grafo);
+    int nodos_visitados = dfs.get_cantidad_nodos_visitados();
+    int cantidad_nodos = grafo.cantidad_nodos();
+    if (hay_ciclo){
       resultado += " FAIL: cycle detected";
       this->resultados.aniadir_dato(resultado);
-    }else if (detector.detecto_instrucciones_sin_utilizar(grafo)){
+    }else if (cantidad_nodos > nodos_visitados){
       resultado += " FAIL: unused instructions detected";
       this->resultados.aniadir_dato(resultado);
     }else{
